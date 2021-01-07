6f50f29218230b0d0b3b5dd3eed0fe6d904dcd89
ivandjordjevic99 <51098172+ivandjordjevic99@users.noreply.github.com>
2021-01-07 20:38:03 +0100

    Added templates and input forms for new movies and comments

--- projekat_django/aplikacija/urls.py
+++ projekat_django/aplikacija/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'aplikacija'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new_movie/', views.new_movie, name='new_movie'),
     path('movies/', views.movies, name='movies'),
     path('movie/<int:id>/', views.movie, name='movie'),
-    path('movie/<int:id>/comments', views.comments, name='comments')
+    path('movie/<int:id>/comments', views.comments, name='comments'),
+    path('movie/<int:id>/new_comment', views.new_comment, name='new_comment')
 ]
--- projekat_django/aplikacija/views.py
+++ projekat_django/aplikacija/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponse
+
+from .forms import CommentForm, MovieForm
 from .models import Movie
 from .models import Comment
 # Create your views here.
@@ -24,5 +26,35 @@ def movie(req, id):
 
 
 def comments(req, id):
-    tmp = Comment.objects.filter(movie = id)
+    tmp = Comment.objects.filter(movie=id)
     return render(req, 'comments.html', {'comments': tmp})
+
+
+def new_comment(req, id):
+    if req.method == 'POST':
+        form = CommentForm(req.POST)
+        if form.is_valid():
+            m = Movie.objects.get(id=id)
+            c = Comment(content=form.cleaned_data['content'], user=req.user, movie=m)
+            c.save()
+            return redirect('aplikacija:comments', id)
+        else:
+            return render(req, 'newComments.html', {'form': form, 'id': id})
+    else:
+        form = CommentForm()
+        return render(req, 'newComment.html', {'form': form, 'id': id})
+
+
+def new_movie(req):
+    if req.method == 'POST':
+        form = MovieForm(req.POST)
+
+        if form.is_valid():
+            m = Movie(name=form.cleaned_data['name'], year=form.cleaned_data['year'], synopsis=form.cleaned_data['synopsis'])
+            m.save()
+            return redirect('aplikacija:movies')
+        else:
+            return render(req, 'newMovie.html', {'form': form})
+    else:
+        form = MovieForm()
+        return render(req, 'newMovie.html', {'form': form})
